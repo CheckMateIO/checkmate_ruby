1fc975a2919d69b2b48c40e12ddac42e1ad94b20
Alex Katkova <alex@checkmate.io>
2014-12-02 15:51:59 -0800

    fixing indentation

--- checkmate_ruby.gemspec
+++ checkmate_ruby.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
-	s.name  = "CheckmateRuby"
-	s.version = "0.0.1"
-	s.files = ["lib/checkmate_ruby.rb", "lib/checkmate_ruby/client.rb"]
-	s.summary = "foobar"
-	s.authors = "kitkat"
+  s.name  = "CheckmateRuby"
+  s.version = "0.0.1"
+  s.files = ["lib/checkmate_ruby.rb", "lib/checkmate_ruby/client.rb"]
+  s.summary = "A Ruby API wrapper for the Checkmate Partner API"
+  s.authors = "Alex Katkova"
 end
--- lib/checkmate_ruby/client.rb
+++ lib/checkmate_ruby/client.rb
@@ -2,58 +2,58 @@ require 'typhoeus'
 require 'json'
 
 module Checkmate
-	class Client
-		include Typhoeus
+  class Client
+    include Typhoeus
 
-		@@base_uri = "https://partners-staging.checkmate.io"
-		attr_writer :private_key
+    @@base_uri = "https://partners-staging.checkmate.io"
+    attr_writer :private_key
 
-		def initialize(private_key)
-			self.private_key = private_key
-		end
+    def initialize(private_key)
+      self.private_key = private_key
+    end
 
-		def get_property(property_params)
-			response = Request.new(
-				properties_uri,
-				method: :get,
-				params: uri_params(property_params),
-				headers: headers,
-				followlocation: true).run
+    def get_property(property_params)
+      response = Request.new(
+        properties_uri,
+        method: :get,
+        params: uri_params(property_params),
+        headers: headers,
+        followlocation: true).run
 
-			handle_response_and_maybe_return_json(response)
-		end
+      handle_response_and_maybe_return_json(response)
+    end
 
-		def headers
-			headers = {
-				"Accept" => "application/json",
-				"X-CheckMate-API-Token" => @private_key
-			}
-		end
+    def headers
+      headers = {
+        "Accept" => "application/json",
+        "X-CheckMate-API-Token" => @private_key
+      }
+    end
 
-		def handle_response_and_maybe_return_json(response)
-			if response.success?
-				JSON.parse(response.body)
-			elsif response.timed_out?
-				"timed out!"
-			elsif response.code == 0
-				response.return_message #something's gone wrong!
-			else
-				response.code.to_s #
-			end
-		end
+    def handle_response_and_maybe_return_json(response)
+      if response.success?
+        JSON.parse(response.body)
+      elsif response.timed_out?
+        "timed out!"
+      elsif response.code == 0
+        response.return_message #something's gone wrong!
+      else
+        response.code.to_s #
+      end
+    end
 
-		def properties_uri
-			@@base_uri + "/properties"
-		end
+    def properties_uri
+      @@base_uri + "/properties"
+    end
 
-		def uri_params(property_params)
-			uri_params = {
-				"property[name]" => property_params[:name],
-				"property[phone]" => property_params[:phone],
-				"property[address][street]" => property_params[:street],
-				"property[address][city]" => property_params[:city],
-				"property[address][region]" => property_params[:region],
-				"property[address][postal_code]" => property_params[:postal_code]}
-		end
-	end
+    def uri_params(property_params)
+      uri_params = {
+        "property[name]" => property_params[:name],
+        "property[phone]" => property_params[:phone],
+        "property[address][street]" => property_params[:street],
+        "property[address][city]" => property_params[:city],
+        "property[address][region]" => property_params[:region],
+        "property[address][postal_code]" => property_params[:postal_code]}
+    end
+  end
 end
