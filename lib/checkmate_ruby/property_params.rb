66964dbb3bfc77f551557278c8c1a278bc315119
Alex Katkova <alex@checkmate.io>
2014-12-04 16:54:20 -0800

    using QueryParams gem to convert property hash to url parameters

--- lib/checkmate_ruby/property_params.rb
+++ lib/checkmate_ruby/property_params.rb
@@ -1,21 +1,15 @@
+require 'queryparams'
+
 module Checkmate
   class PropertyParams
-    attr_accessor :name, :phone, :street, :city, :region, :postal_code
+    attr_accessor :property_params
     
     def initialize(property_params)
-      property_params.each do |key, value|
-        public_send("#{key}=", value)
-      end
+      self.property_params = property_params
     end
 
     def to_uri_params
-      uri_params = {
-        "property[name]" => name,
-        "property[phone]" => phone,
-        "property[address][street]" => street,
-        "property[address][city]" => city,
-        "property[address][region]" => region,
-        "property[address][postal_code]" => postal_code}
+      QueryParams.encode({:property => property_params}) 
     end
 
     def uri_path
--- spec/unit/property_params_spec.rb
+++ spec/unit/property_params_spec.rb
@@ -1,8 +1,9 @@
 require_relative '../spec_helper'
+require 'uri'
 
 describe Checkmate::PropertyParams do
-  let(:property) {Checkmate::PropertyParams.new({:name => "n", :phone => "123",
-      :street => "1625", :city => "San", :region => "CA", :postal_code => "94115"})}
+  let(:property) {Checkmate::PropertyParams.new({:name => "n%&", :phone => "123",
+      :address => {:street => "1625", :city => "San Fr", :region => "CA", :postal_code => "94115"}})}
 
   it "returns correct path" do 
     expect(property.uri_path).to eq("/properties")
@@ -10,11 +11,6 @@ describe Checkmate::PropertyParams do
 
   it "converts to uri params" do
     uri_params = property.to_uri_params
-    expect(uri_params["property[name]"]).to eq(property.name)
-    expect(uri_params["property[phone]"]).to eq(property.phone)
-    expect(uri_params["property[address][street]"]).to eq(property.street)
-    expect(uri_params["property[address][city]"]).to eq(property.city)
-    expect(uri_params["property[address][region]"]).to eq(property.region)
-    expect(uri_params["property[address][postal_code]"]).to eq(property.postal_code)
+    # it didn't blow up!
   end
 end
